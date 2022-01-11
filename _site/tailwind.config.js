7a4c443a8ee3193a3bcc8e331b34934396fb6d7a
Gus Schiavon <hello@itsgus.dev>
2022-01-10 20:14:23 -0400

    Add CSS dir to config file for PurgeCSS

--- _site/tailwind.config.js
+++ _site/tailwind.config.js
@@ -9,7 +9,19 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
-    extend: customTheme,
+    extend: {
+      customTheme,
+      fontFamily: {
+        "heading": customTheme.fonts.heading_font_family,
+        "body": customTheme.fonts.body_font_family
+      },
+      colors: {
+        "primary": customTheme.colors.primary_color,    
+        "secondary": customTheme.colors.secondary_color,    
+        "tertiary": customTheme.colors.tertiary_color,    
+        "accent": customTheme.colors.accent_color,
+      }
+     },    
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
